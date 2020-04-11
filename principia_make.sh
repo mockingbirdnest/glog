d07df78c0a6bc1eb99e8c9f6e7c01ac8269a3002
Alex Wang <ts826848@gmail.com>
2020-04-11 15:50:56 -0400

    Match output locations from autotools build
    This should avoid the need to make any changes to the Principia Makefile
    and Azure pipeline config.
    
    The important artifacts are the generated headers and libglog.a. The
    autotools build generates the former in src/glog/ and the latter at
    .libs/. Since the CMake build uses
    
        configure_file(<header.in> glog/<header> @ONLY)
    
    to generate the header files, and the output location is relative to
    CMAKE_CURRENT_BINARY_DIR, an in-tree build rooted at src/ must be used
    to match the header output location from the autotools build. Luckily,
    the static library output location is controlled by a separate variable,
    so ensuring libglog.a is in the right place is quite straightforward.

--- principia_make.sh
+++ principia_make.sh
@@ -1,5 +1,4 @@
-mkdir -p build
-pushd build
+pushd src
 cmake \
     -DCMAKE_C_COMPILER:FILEPATH="$(which clang)" \
     -DCMAKE_CXX_COMPILER:FILEPATH="$(which clang++)" \
@@ -7,6 +6,7 @@ cmake \
     -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" \
     -DWITH_GFLAGS=OFF \
     -DBUILD_SHARED_LIBRARIES=OFF \
+    -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:FILEPATH=../.libs \
     ..
 make -j8
 popd

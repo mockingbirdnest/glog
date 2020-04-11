7ee4357ff61cb88c5f3023a624848d8614b48219
Alex Wang <ts826848@gmail.com>
2020-04-11 16:47:07 -0400

    Error out if C_FLAGS/CXX_FLAGS is not set
    Match original behavior

--- principia_make.sh
+++ principia_make.sh
@@ -2,8 +2,8 @@ pushd src
 cmake \
     -DCMAKE_C_COMPILER:FILEPATH="$(which clang)" \
     -DCMAKE_CXX_COMPILER:FILEPATH="$(which clang++)" \
-    -DCMAKE_C_FLAGS="${C_FLAGS}" \
-    -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" \
+    -DCMAKE_C_FLAGS="${C_FLAGS?}" \
+    -DCMAKE_CXX_FLAGS="${CXX_FLAGS?}" \
     -DWITH_GFLAGS=OFF \
     -DBUILD_SHARED_LIBRARIES=OFF \
     -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:FILEPATH=../.libs \
